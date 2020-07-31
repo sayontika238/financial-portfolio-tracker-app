f1dfc07fe4a9ac3c62aceafc91f5a4207585f626
sayontika238 <konarsayontika560@gmail.com>
2020-07-31 09:49:25 +0530

    Create config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAZuwEbU-iE6TyIncBy9n3BYMMQRSELBiM",
+    authDomain: "financial-portpfolio-tracker.firebaseapp.com",
+    databaseURL: "https://financial-portpfolio-tracker.firebaseio.com",
+    projectId: "financial-portpfolio-tracker",
+    storageBucket: "financial-portpfolio-tracker.appspot.com",
+    messagingSenderId: "569439264396",
+    appId: "1:569439264396:web:2492e5dee7bf13277465a9",
+    measurementId: "G-LYHDBRFH94"
+  };
