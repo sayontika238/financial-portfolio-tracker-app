3bd5638b089cec279b5bb90e9468dafd8be16527
sayontika238 <konarsayontika560@gmail.com>
2020-08-10 20:30:57 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,6 @@
-var firebaseConfig = {
+import firebase from 'firebase';
+import 'firebase/auth';
+export var firebaseConfig = {
     apiKey: "AIzaSyAZuwEbU-iE6TyIncBy9n3BYMMQRSELBiM",
     authDomain: "financial-portpfolio-tracker.firebaseapp.com",
     databaseURL: "https://financial-portpfolio-tracker.firebaseio.com",
@@ -8,3 +10,5 @@ var firebaseConfig = {
     appId: "1:569439264396:web:2492e5dee7bf13277465a9",
     measurementId: "G-LYHDBRFH94"
   };
+  export const firebaseApp = firebase.intializeApp(firebaseConfig)
+  export default firebaseApp.firestore();
